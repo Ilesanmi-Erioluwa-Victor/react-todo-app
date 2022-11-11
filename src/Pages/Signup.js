501f6eac5ed165208e91203e830d6d3175766ec9
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 23:53:40 +0100

    Creating my switch statement

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -45,8 +45,11 @@ const initialState = {
 };
 
 const reducer = (state, action) {
-  
+    switch(action.type) {
+      case "name" :
+    }
 }
+
 export const Signup = () => {
   return <div>Signup</div>;
 };
