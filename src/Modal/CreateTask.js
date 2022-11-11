36c48d05dffeaf6ba558c16b777ff39e993a6ab1
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 18:26:25 +0100

    working on Modal

--- src/Modal/CreateTask.js
+++ src/Modal/CreateTask.js
@@ -27,3 +27,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import { createTask, getTask } from "../Redux/AppReducer/action";
+
+const initialState = {
+  title: "",
+  description: "Default Description",
+  task_status: "todo",
+  tags: ["Others"],
+  subTasks: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "title":
+      return {
+        ...state,
+        title: action.payload,
+      };
+
+    case "description":
+      return {
+        ...state,
+        description: action.payload,
+      };
+    case "task_status":
+      return {
+        ...state,
+        task_status: action.payload,
+      };
+    case "tags":
+      return {
+        ...state,
+        tags: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const CreateTask = ({ isOpen, onClose }) => {
+  const [state, setState] = useReducer(reducer, initialState);
+  const dispatch = useDispatch();
+};
