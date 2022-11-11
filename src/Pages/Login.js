cb1878b0ea3fa58d1461c8e4dcf4060ab5e69e12
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 23:26:57 +0100

    Working on Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,99 @@
-import React from "react";
+import { ViewIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Link,
+  Stack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link as RouterLink, useLocation, useNavigate } from "react-router-dom";
+import { login } from "../Redux/AuthReducer/action";
 
 export const Login = () => {
-  return <div>Login</div>;
+  
+   return (
+     <Flex minH={"100vh"} align={"center"} justify={"center"}>
+       <Stack
+         spacing={8}
+         mx={"auto"}
+         maxW={"lg"}
+         py={12}
+         px={6}
+         color={"white"}
+         borderRadius={"2rem"}
+         bg={"#436c89"}
+       >
+         <Stack align={"center"}>
+           <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+         </Stack>
+         <Box rounded={"lg"} boxShadow={"lg"} p={8}>
+           <Stack spacing={4}>
+             <FormControl id="username" isRequired>
+               <FormLabel>Username</FormLabel>
+               <Input
+                 type="text"
+                 value={username}
+                 onChange={(e) => setUsername(e.target.value)}
+               />
+             </FormControl>
+             <FormControl id="password" isRequired>
+               <FormLabel>Password</FormLabel>
+               <InputGroup>
+                 <Input
+                   type={eye ? "text" : "password"}
+                   value={password}
+                   onChange={(e) => setPassword(e.target.value)}
+                 />
+                 <InputRightElement h={"full"}>
+                   <Button variant={"ghost"} onClick={handleEye}>
+                     <ViewIcon />
+                   </Button>
+                 </InputRightElement>
+               </InputGroup>
+             </FormControl>
+             <Stack spacing={10}>
+               <Stack
+                 direction={{ base: "column", sm: "row" }}
+                 align={"start"}
+                 justify={"space-between"}
+               >
+                 <Checkbox>Remember me</Checkbox>
+                 <Link color={"#2cb2ba"}>Forgot password?</Link>
+               </Stack>
+               <Button
+                 bg={"#e4e7e4"}
+                 color={"black"}
+                 _hover={{
+                   bg: "white",
+                 }}
+                 onClick={loginHandler}
+               >
+                 Sign in
+               </Button>
+             </Stack>
+             <Stack pt={6}>
+               <Text align={"center"}>
+                 Don't have an account?{" "}
+                 <RouterLink to="/signup" color={"blue.400"}>
+                   Signup
+                 </RouterLink>
+               </Text>
+             </Stack>
+           </Stack>
+         </Box>
+       </Stack>
+     </Flex>
+   );
 };
