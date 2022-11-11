1327bb0191ad34b9e5698b240330c1fa05a12188
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 23:47:06 +0100

    Working on Sign in

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,4 +1,38 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Editable,
+  EditablePreview,
+  EditableTextarea,
+  Divider,
+  useToast,
+  Spinner,
+} from "@chakra-ui/react";
+import { ViewIcon } from "@chakra-ui/icons";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../Redux/AuthReducer/action";
+
+import {
+  checkCharacter,
+  checkEmail,
+  checkMobile,
+  checkPassword,
+  checkSignupForm,
+  setToast,
+} from "../utils/Authenticate";
 
 export const Signup = () => {
   return <div>Signup</div>;
