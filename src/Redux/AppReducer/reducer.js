14bfccdfc6f87f1995c6cfa78eca41c5e5254d17
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 17:56:34 +0100

    Working on my reducer.js in AppReducer folder

---
+++ src/Redux/AppReducer/reducer.js
@@ -0,0 +1,25 @@
+import * as types from "./actionTypes";
+
+const initilaState = {
+    tasks : [],
+    tags : [],
+    isLoading : false,
+    isError : false
+};
+
+const reducer = ( state = initilaState, action) => {
+    const {type, payload } = action;
+
+    switch(type) {
+        case types.GET_TASKS_REQUEST:
+            return {...state, isLoading : true}
+
+            case types.GET_TASKS_SUCCESS:
+                return { ...state, isLoading: false, tasks: payload}
+
+                case types.GET_TASKS_FAILURE:
+                    return {...state, isLoading : false, isError : true}
+                    
+                    case types.GET_TAG_REQUEST : 
+    }
+}
