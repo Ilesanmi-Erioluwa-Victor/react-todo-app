623b36b7a803b62965fa4a1bc35b03d78520ce15
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 18:40:03 +0100

    Working on PrivateRoute

--- src/PrivateRoute/PrivateRoute.js
+++ src/PrivateRoute/PrivateRoute.js
@@ -3,5 +3,6 @@ import { Navigate, useLocation } from "react-router-dom";
 
 
 const PrivateRoute = ({children}) => {
-
+   const location = useLocation();
+   const auth = useSelector( state => state.AuthReducer.isAuth);
 }
