6933098493a53907eba3c6633c0e4aea08e5ac72
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 17:16:32 +0100

    Saved info to LocalStorage

---
+++ src/util/LocalStorage.js
@@ -0,0 +1,14 @@
+const getLocalKey = (key) => {
+  if (key) {
+    const data = localStorage.getItem(key);
+    return data;
+  }
+};
+
+const saveToLocalStorage = (key, value) => {
+  if (key && value) {
+    localStorage.setItem(key, value);
+  }
+};
+
+export {getLocalKey, saveToLocalStorage}
