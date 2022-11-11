2a0128ff4c7db5e849814b09316930a2dfa21332
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 23:44:56 +0100

    Done with Login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -22,105 +22,102 @@ import { Link as RouterLink, useLocation, useNavigate } from "react-router-dom";
 import { login } from "../Redux/AuthReducer/action";
 
 export const Login = () => {
-  
   const dispatch = useDispatch();
   const toast = useToast();
   const navigate = useNavigate();
   const location = useLocation();
   const comingFrom = location.state?.from?.pathname || "/";
-  const [username, setUsername ] = useState("");
-  const [password, setPassword ] = useState("");
-  const [ eye, setEye ] = useState (false);
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [eye, setEye] = useState(false);
 
   const handleEye = () => {
-    setEye((prev) => !prev)
-  }
+    setEye((prev) => !prev);
+  };
 
   const loginHandler = () => {
-    if( username && password) {
+    if (username && password) {
       const params = {
         username,
-        password
+        password,
       };
-          dispatch(login(params, toast)).then((r) => {
-            navigate(comingFrom, { replace: true });
-          });
-    }  
-  }
+      dispatch(login(params, toast)).then((r) => {
+        navigate(comingFrom, { replace: true });
+      });
+    }
+  };
 
-
-   return (
-     <Flex minH={"100vh"} align={"center"} justify={"center"}>
-       <Stack
-         spacing={8}
-         mx={"auto"}
-         maxW={"lg"}
-         py={12}
-         px={6}
-         color={"white"}
-         borderRadius={"2rem"}
-         bg={"#436c89"}
-       >
-         <Stack align={"center"}>
-           <Heading fontSize={"4xl"}>Sign in to your account</Heading>
-         </Stack>
-         <Box rounded={"lg"} boxShadow={"lg"} p={8}>
-           <Stack spacing={4}>
-             <FormControl id="username" isRequired>
-               <FormLabel>Username</FormLabel>
-               <Input
-                 type="text"
-                 value={username}
-                 onChange={(e) => setUsername(e.target.value)}
-               />
-             </FormControl>
-             <FormControl id="password" isRequired>
-               <FormLabel>Password</FormLabel>
-               <InputGroup>
-                 <Input
-                   type={eye ? "text" : "password"}
-                   value={password}
-                   onChange={(e) => setPassword(e.target.value)}
-                 />
-                 <InputRightElement h={"full"}>
-                   <Button variant={"ghost"} onClick={handleEye}>
-                     <ViewIcon />
-                   </Button>
-                 </InputRightElement>
-               </InputGroup>
-             </FormControl>
-             <Stack spacing={10}>
-               <Stack
-                 direction={{ base: "column", sm: "row" }}
-                 align={"start"}
-                 justify={"space-between"}
-               >
-                 <Checkbox>Remember me</Checkbox>
-                 <Link color={"#2cb2ba"}>Forgot password?</Link>
-               </Stack>
-               <Button
-                 bg={"#e4e7e4"}
-                 color={"black"}
-                 _hover={{
-                   bg: "white",
-                 }}
-                 onClick={loginHandler}
-               >
-                 Sign in
-               </Button>
-             </Stack>
-             <Stack pt={6}>
-               <Text align={"center"}>
-                 Don't have an account?{" "}
-                 <RouterLink to="/signup" color={"blue.400"}>
-                   Signup
-                 </RouterLink>
-               </Text>
-             </Stack>
-           </Stack>
-         </Box>
-       </Stack>
-     </Flex>
-   );
+  return (
+    <Flex minH={"100vh"} align={"center"} justify={"center"}>
+      <Stack
+        spacing={8}
+        mx={"auto"}
+        maxW={"lg"}
+        py={12}
+        px={6}
+        color={"white"}
+        borderRadius={"2rem"}
+        bg={"#436c89"}
+      >
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+        </Stack>
+        <Box rounded={"lg"} boxShadow={"lg"} p={8}>
+          <Stack spacing={4}>
+            <FormControl id="username" isRequired>
+              <FormLabel>Username</FormLabel>
+              <Input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={eye ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputRightElement h={"full"}>
+                  <Button variant={"ghost"} onClick={handleEye}>
+                    <ViewIcon />
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Remember me</Checkbox>
+                <Link color={"#2cb2ba"}>Forgot password?</Link>
+              </Stack>
+              <Button
+                bg={"#e4e7e4"}
+                color={"black"}
+                _hover={{
+                  bg: "white",
+                }}
+                onClick={loginHandler}
+              >
+                Sign in
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Don't have an account?{" "}
+                <RouterLink to="/signup" color={"blue.400"}>
+                  Signup
+                </RouterLink>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 };
