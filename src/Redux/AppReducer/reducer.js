f326c6d7d25d7e640bd003d5ff5da187df02ca32
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 18:02:21 +0100

    Done with AppReducer

--- src/Redux/AppReducer/reducer.js
+++ src/Redux/AppReducer/reducer.js
@@ -10,20 +10,28 @@ const initilaState = {
 const reducer = ( state = initilaState, action) => {
     const {type, payload } = action;
 
-    switch(type) {
-        case types.GET_TASKS_REQUEST:
-            return {...state, isLoading : true}
+    switch (type) {
+      case types.GET_TASKS_REQUEST:
+        return { ...state, isLoading: true };
 
-            case types.GET_TASKS_SUCCESS:
-                return { ...state, isLoading: false, tasks: payload}
+      case types.GET_TASKS_SUCCESS:
+        return { ...state, isLoading: false, tasks: payload };
 
-                case types.GET_TASKS_FAILURE:
-                    return {...state, isLoading : false, isError : true}
-                    
-                    case types.GET_TAG_REQUEST :
-                        return {...state}
-                        
-                        case types.GET_TAG_SUCCESS:
-                            return{...state, isLoading : false, tag : payload}
+      case types.GET_TASKS_FAILURE:
+        return { ...state, isLoading: false, isError: true };
+
+      case types.GET_TAG_REQUEST:
+        return { ...state };
+
+      case types.GET_TAG_SUCCESS:
+        return { ...state, tag: payload };
+
+      case types.GET_TAG_FAILURE:
+        return { ...state };
+
+      default:
+        return state;
     }
 }
+
+export { reducer}
