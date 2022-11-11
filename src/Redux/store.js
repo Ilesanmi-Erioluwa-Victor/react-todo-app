1a38a0d5700f9c1c71f30aabe96d9735c005327e
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-11 15:16:55 +0100

    Working on my AuthReducer in Redux folder

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,10 @@
 import { legacy_createStore,combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-
+import { reducer as AppReducer } from "./AppReducer/reducer"
+import { reducer as AuthReducer } from "./AppReducer/reducer";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const rootReducers = combineReducers({  })
+const rootReducers = combineReducers({ AppReducer,AuthReducer })
 
 const store = legacy_createStore(
  rootReducers,
