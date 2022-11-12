5a9a5292320e31bbbe5fa673c3ba32f14a1b01cf
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-12 01:37:32 +0100

    Started working on EditPage

--- src/Pages/EditPage.js
+++ src/Pages/EditPage.js
@@ -16,6 +16,40 @@ import {
   Text,
   useDisclosure,
 } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import CreateTask from "../Modal/CreateTask";
+
+import {
+  addSubTasks,
+  addTag,
+  deleteSubTask,
+  getTagsList,
+  getTasks,
+  updateSubtasksList,
+  updateTasks,
+} from "../Redux/AppReducer/action";
+
+import { UPDATE_TASK_SUCCESS } from "../Redux/AppReducer/actionTypes";
+
+const date = new Date().toLocaleDateString();
+
 export const EditPage = () => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { id } = useParams();
+    const tasks = useSelector((store) => store.AppReducer.tasks);
+    const tagList = useSelector((store) => store.AppReducer.tags);
+    const dispatch = useDispatch();
+
+      const [taskTitle, setTaskTitle] = useState("");
+      const [taskDescription, setTaskDescription] = useState("");
+      const [taskStatus, setTaskStatus] = useState("");
+      const [taskTags, setTaskTags] = useState([]);
+      const [subTasks, setSubTasks] = useState([]);
+      const [checkBox, setCheckBox] = useState([]);
+      const [currentSubTask, setCurrentSubTask] = useState("");
+      const [newTag, setNewTag] = useState("");
   return <div>EditPage</div>;
 };
