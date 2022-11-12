b1dbef2119e63822d4d85241e8e3beb086d7b286
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-12 01:19:29 +0100

    Working on Homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import TaskCard from "../Components/TaskCard";
+import { getTasks } from "../Redux/AppReducer/action";
 
 export const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+const tasks = useSelector( (store) =>store.AppReducer.tasks);
+const searchParams = useSearchParams();
+
+  return <div>HomePage</div>;
+};
